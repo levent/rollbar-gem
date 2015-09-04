9980beef48d1cad3f235be55dbaa401d466a1bd8
Jon de Andres <jon@rollbar.com>
2015-09-04 12:33:29 +0200

    Send nearest backtrace entry on failsafe messages. @brianr.

--- spec/rollbar_spec.rb
+++ spec/rollbar_spec.rb
@@ -1564,14 +1564,14 @@ describe Rollbar do
   context "send_failsafe" do
     let(:exception) { StandardError.new }
 
-    it "should not crash when given a message and exception" do
+    it "doesn't crash when given a message and exception" do
       sent_payload = notifier.send(:send_failsafe, "test failsafe", exception)
 
       expected_message = 'Failsafe from rollbar-gem: StandardError: test failsafe'
       expect(sent_payload['data'][:body][:message][:body]).to be_eql(expected_message)
     end
 
-    it "should not crash when given all nils" do
+    it "doesn't crash when given all nils" do
       notifier.send(:send_failsafe, nil, nil)
     end
 
@@ -1580,6 +1580,19 @@ describe Rollbar do
         sent_payload = notifier.send(:send_failsafe, "test failsafe", nil)
 
         expected_message = 'Failsafe from rollbar-gem: test failsafe'
+        expect(sent_payload['data'][:body][:message][:body]).to be_eql(expected_message)
+      end
+    end
+
+    context 'if the exception has a backtrace' do
+      let(:backtrace) { ['func3', 'func2', 'func1'] }
+
+      before { exception.set_backtrace(backtrace) }
+
+      it 'adds the nearest frame to the message' do
+        sent_payload = notifier.send(:send_failsafe, "test failsafe", exception)
+
+        expected_message = 'Failsafe from rollbar-gem: StandardError in func3: test failsafe'
         expect(sent_payload['data'][:body][:message][:body]).to be_eql(expected_message)
       end
     end
